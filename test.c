ed1864f0ffb36c3888868365c260ecda74fd7eb2
samueleishion <samueleishion@gmail.com>
2014-02-16 15:36:15 -0600

    Added test from piazza

--- test.c
+++ test.c
@@ -1,62 +1,58 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <inttypes.h> 
 #include <setjmp.h> 
-#include "threads.h" 
-
-void f4(void *arg) {
-  int i; 
-  while(1) {
-    printf("thread 4: %d\n", i++); 
-    thread_yield(); 
-  }
+#include "threads.h"
+ 
+void f2(void*);
+void f3(void*);
+void f4(void*);
+ 
+void f1(void* arg){
+  printf("f1\n");
+  printf("creating thread 2 in f1\n");
+  struct thread *t2 = thread_create(f2, NULL);
+  thread_add_runqueue(t2);
+  printf("yielding in f1\n");
+  thread_yield();
+  printf("finishing f1 after yield\n");
 }
-
-void f3(void *arg) {
-  int i; 
-  while (1) {
-    printf("thread 3: %d\n", i++);
-    if(i==100) {
-      i = 0; 
-    }
-    thread_yield(); 
-  }
-} 
-
-void f2(void *arg) {
-  int i; 
-  while(1) {
-    printf("thread 2: %d\n",i++); 
-    if(i==10) {
-      i = 0; 
-    }
-    thread_yield(); 
-  }
+ 
+void f2(void* arg){
+  printf("f2\n");
+  printf("creating thread 3 in f2\n");
+  struct thread *t3 = thread_create(f3, NULL);
+  thread_add_runqueue(t3);    
+  printf("yielding in f2\n");    
+  thread_yield();
+  printf("finishing f2 after yield\n");
+ 
+  //thread_exit();
 }
-
-
-void f1(void*arg) {
-  int i = 100; 
-  struct thread *t2 = thread_create(f2, NULL); 
-  struct thread *t3 = thread_create(f3, NULL); 
-  struct thread *t4 = thread_create(f4, NULL); 
-
-  thread_add_runqueue(t4); 
-  thread_add_runqueue(t3); 
-  thread_add_runqueue(t2); 
-
-  while(1) {
-    printf("thread 1: %d\n",i++); 
-    if(i==110) {
-      i = 100; 
-    }
-    thread_yield(); 
-  }
+ 
+void f3(void* arg){
+  printf("f3\n");
+  printf("creating thread 4 in f3\n");    
+  struct thread *t4 = thread_create(f4, NULL);
+  thread_add_runqueue(t4);  
+  printf("yielding in f3\n");
+  thread_yield();
+  printf("finishing f3 after yield\n");
 }
-
-int main(int argc, char **argv) {
-  struct thread *t1 = thread_create(f1, NULL); 
-  thread_add_runqueue(t1); 
-  thread_start_threading(); 
-  return 0; 
-} 
-      
+ 
+void f4(void* arg){
+  printf("f4\n");
+}
+ 
+int main(int argc, char **argv)
+{
+  printf("creating thread 1 in main\n");
+  struct thread *t1 = thread_create(f1, NULL);
+  thread_add_runqueue(t1);
+ 
+  printf("***** start threading *****\n");
+  thread_start_threading();
+  printf("\nexited\n");
+  return 0;
+}
